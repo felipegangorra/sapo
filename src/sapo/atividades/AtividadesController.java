dcf4a094e8c1edc270b340961f470d01a14cbc13
Letícia Farias <100238247+LeticiaFarias21@users.noreply.github.com>
2022-08-11 18:09:44 -0300

    Add atividades, modificaçao nome do package de sapo.model para sapo.pessoa (#6)

---
+++ src/sapo/atividades/AtividadesController.java
@@ -0,0 +1,25 @@
+package sapo.atividades;
+
+public class AtividadesController {
+
+    private AtividadeService ats;
+
+    public AtividadesController(AtividadeService ats){
+
+        this.ats = ats;
+    }
+
+    public String cadastrarAtividade(String nome , String descricao, String cpf){
+        //
+        return null;
+    }
+
+//+ adicionaAtividade(Atividades : at): void
+//+ encerrarAtividade(AtividadeId: str): void
+//+ desativarAtividade(atividadeId: str): void
+//+ reabrirAtividade(atividadeId: str): void
+//+ exibirAtividade(atividadeId: str): str
+//+ alterarDescricaoAtividade(atividadeId: str, descricao: str): void
+//+ alterarResponsavelAtividade(atividadeId: str, cpf: str): void
+
+}
