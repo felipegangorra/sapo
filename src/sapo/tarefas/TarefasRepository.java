89979ee8487a14e39bf5d9c286dc9692b383caaa
gangorra <felipegangorra@gmail.com>
2022-08-12 16:17:35 -0300

    atualizando tarefas

--- src/sapo/tarefas/TarefasRepository.java
+++ src/sapo/tarefas/TarefasRepository.java
@@ -3,39 +3,39 @@ package sapo.tarefas;
 public class TarefasRepository {
 
 	public void cadastrar(String atividadeId, String nome, String[] habilidades) {
-		return;
+		
 	}
 
 	public void alterarNome(String idTarefa, String novoNome) {
-		return;
+		
 	}
 
 	public void alterarHabilidade(String idTarefa, String[] habilidades) {
-		return;
+		
 	}
 
 	public void adicionarHoras(String idTarefa, int horas) {
-		return;
+		
 	}
 
 	public void removeHoras(String idTarefa, int horas) {
-		return;
+		
 	}
 
 	public void concluir(String idTarefa) {
-		return;
+		
 	}
 
 	public void remover(String idTarefa) {
-		return;
+		
 	}
 
 	public void associarPessoa(String cpf, String idTarefa) {
-		return;
+		
 	}
 
 	public void removerPessoa(String cpf, String idTarefa) {
-		return;
+		
 	}
 
 	public String exibir(String idTarefa) {
