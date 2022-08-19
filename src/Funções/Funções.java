0c7ec99a879058cf361ff5a1da5b4acbe9cb34c9
Leticia Farias <leticiafariasvieira@gmail.com>
2022-08-19 16:15:19 -0300

    add Funções

---
+++ src/Funções/Funções.java
@@ -0,0 +1,13 @@
+package Funções;
+
+import Sapo.Pessoa.Pessoa;
+
+import java.util.List;
+import java.util.Set;
+
+public class Funções extends Pessoa {
+    public Funções(String cpf, String nome, Set<String> habilidades, List<String> comentarios) {
+        super(cpf, nome, habilidades, comentarios);
+    }
+
+}
